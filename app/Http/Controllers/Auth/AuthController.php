7ac8ee354169b9e1928fc322dea97df396f333b4
Sergio Yucra <sergio22yucra@gmail.com>
2024-09-03 15:28:09 -0400

    Backend register and login

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credenciales = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credenciales)) {
+                return response()->json([
+                    'error' => 'credenciales no validas',
+                ], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'no se creo el token',
+            ], 500);
+        }
+        return response()->json(compact('token'), 200);
+    }
+}
